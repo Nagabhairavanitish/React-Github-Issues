b41629f4dd08dcdb9e8db6fee5e74692426da764
Nagabhairavanitish <nitishrockson@gmail.com>
2017-07-30 10:39:21 +0530

    Version 1.0

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import './App.css';
+import { gql, graphql } from 'react-apollo';
+
+class App extends Component {
+
+  renderEl(nodes) {
+    if (nodes.title) {
+      const bodyText = nodes.bodyText.substring(0,250);
+      return (
+        <div className="card">
+          <div className="card-block">
+            <h4 className="card-title">{nodes.title}</h4>
+            <p className="card-text">
+            <div className="row">
+              <div className="col-3">
+                <img className="avatar" alt="avatar" src={nodes.author.avatarUrl}/>
+              </div>
+              <div className="col-9 bodyText">
+                {bodyText} - <a href={nodes.url}>Read More</a>
+              </div>
+            </div>
+            </p>
+            <p><b>Created at: </b>{nodes.createdAt}</p>
+          </div>
+        </div>
+      );
+    }
+  }
+  render() {
+    const { loading, error, search } = this.props;
+    if (loading) return <div>Loading</div>;
+    if (error) return <h1>Error</h1>;
+    return (
+      <div>
+      <h2>Javascript Issues</h2>
+      {search.nodes.map(nodes => (
+        this.renderEl(nodes)
+    ))}
+    </div>
+
+    );
+  }
+}
+
+const FeedQuery = gql`
+{
+  search(query: "language:JavaScript", type: ISSUE, first: 20) {
+    nodes {
+      ... on Issue {
+        createdAt
+        title
+        url
+        bodyText
+        repository {
+          url
+        }
+        author {
+          avatarUrl
+          url
+        }
+      }
+    }
+  }
+}
+`
+
+const FeedWithData = graphql(FeedQuery,
+  {props: ({ data }) => ({ ...data })
+})(App);
+
+
+export default FeedWithData;
---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { ApolloProvider, IntrospectionFragmentMatcher } from 'react-apollo';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import registerServiceWorker from './registerServiceWorker';
+
+const networkInterface = createNetworkInterface({ uri: 'https://api.github.com/graphql'});
+
+const token = '15f1cc045377ffe8e7d5bdcd2d878c19ff60385c';
+
+const middlewareOne = {
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {};  // Create the header object if needed.
+    }
+    req.options.headers.authorization = `Bearer ${token}`;
+    next();
+  }
+}
+
+networkInterface.use([middlewareOne]);
+
+const FragmentMatcher = new IntrospectionFragmentMatcher({
+  introspectionQueryResultData: {
+    __schema: {
+      types: [
+        {
+          kind: "OBJECT",
+          name: "Issue",
+        }, // this is just an example, put your own INTERFACE and UNION types here!
+      ],
+    },
+  }
+})
+
+const client = new ApolloClient({
+  networkInterface: networkInterface,
+  fragmentMatcher: FragmentMatcher,
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>,
+  document.getElementById('root')
+);registerServiceWorker();
